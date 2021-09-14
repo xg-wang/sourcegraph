e59971fb923cd5dc27fc1ff89bfdf9d594fdc91b
Chris Wendt <chrismwendt@gmail.com>
2021-09-14 00:38:38 -0600

    wip on extension host

--- client/shared/src/api/extension/extensionHostState.ts
+++ client/shared/src/api/extension/extensionHostState.ts
@@ -52,6 +52,9 @@ export function createExtensionHostState(
         >([]),
         definitionProviders: new BehaviorSubject<readonly RegisteredProvider<sourcegraph.DefinitionProvider>[]>([]),
         referenceProviders: new BehaviorSubject<readonly RegisteredProvider<sourcegraph.ReferenceProvider>[]>([]),
+        implementationProviders: new BehaviorSubject<readonly RegisteredProvider<sourcegraph.ImplementationProvider>[]>(
+            []
+        ),
         locationProviders: new BehaviorSubject<
             readonly RegisteredProvider<{ id: string; provider: sourcegraph.LocationProvider }>[]
         >([]),
@@ -124,6 +127,7 @@ export interface ExtensionHostState {
     documentHighlightProviders: BehaviorSubject<readonly RegisteredProvider<sourcegraph.DocumentHighlightProvider>[]>
     definitionProviders: BehaviorSubject<readonly RegisteredProvider<sourcegraph.DefinitionProvider>[]>
     referenceProviders: BehaviorSubject<readonly RegisteredProvider<sourcegraph.ReferenceProvider>[]>
+    implementationProviders: BehaviorSubject<readonly RegisteredProvider<sourcegraph.ImplementationProvider>[]>
     locationProviders: BehaviorSubject<
         readonly RegisteredProvider<{ id: string; provider: sourcegraph.LocationProvider }>[]
     >
