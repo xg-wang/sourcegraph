ec3517c375f6c867cb26455fc35302f2c6db21c9
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-10-12 16:13:37 +0400

    go code modified: failing test, should run go tests

--- internal/database/dbconn/dbconn_test.go
+++ internal/database/dbconn/dbconn_test.go
@@ -15,7 +15,7 @@ func TestBuildConfig(t *testing.T) {
 			name:                    "empty dataSource",
 			dataSource:              "",
 			expectedApplicationName: defaultApplicationName,
-			fails:                   false,
+			fails:                   true,
 		}, {
 			name:                    "connection string",
 			dataSource:              "dbname=sourcegraph host=localhost sslmode=verify-full user=sourcegraph",
