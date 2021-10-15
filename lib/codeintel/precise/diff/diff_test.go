2ad02fd20308ddc7ffe1662fd6b59a273e17923e
Chris Wendt <chrismwendt@gmail.com>
2021-10-14 20:04:09 -0600

    lint

--- lib/codeintel/precise/diff/diff_test.go
+++ lib/codeintel/precise/diff/diff_test.go
@@ -5,16 +5,12 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/hexops/autogold"
-
 	"github.com/sourcegraph/sourcegraph/lib/codeintel/lsif/conversion"
 	"github.com/sourcegraph/sourcegraph/lib/codeintel/precise"
 )
 
 var dumpPath = "./testdata/project1/dump.lsif"
 var dumpPermutedPath = "./testdata/project1/dump-permuted.lsif"
-var dumpOldPath = "./testdata/project1/dump-old.lsif"
-var dumpNewPath = "./testdata/project1/dump-new.lsif"
 
 func TestNoDiffOnPermutedDumps(t *testing.T) {
 	bundle1, err := conversion.CorrelateLocalGit(
@@ -40,31 +36,31 @@ func TestNoDiffOnPermutedDumps(t *testing.T) {
 	}
 }
 
-func TestDiffOnEditedDumps(t *testing.T) {
-	// Flaky because Diff() is nondeterministic
-	t.Skip()
-	bundle1, err := conversion.CorrelateLocalGit(
-		context.Background(),
-		dumpOldPath,
-		filepath.Dir(dumpOldPath),
-	)
-	if err != nil {
-		t.Fatalf("Unexpected error reading dump: %v", err)
-	}
+// Flaky because Diff() is nondeterministic
+// t.Skip() causes lint errors, so comment the whole test out
+// func TestDiffOnEditedDumps(t *testing.T) {
+// 	bundle1, err := conversion.CorrelateLocalGit(
+// 		context.Background(),
+// 		dumpOldPath,
+// 		filepath.Dir(dumpOldPath),
+// 	)
+// 	if err != nil {
+// 		t.Fatalf("Unexpected error reading dump: %v", err)
+// 	}
 
-	bundle2, err := conversion.CorrelateLocalGit(
-		context.Background(),
-		dumpNewPath,
-		filepath.Dir(dumpNewPath),
-	)
-	if err != nil {
-		t.Fatalf("Unexpected error reading dump: %v", err)
-	}
+// 	bundle2, err := conversion.CorrelateLocalGit(
+// 		context.Background(),
+// 		dumpNewPath,
+// 		filepath.Dir(dumpNewPath),
+// 	)
+// 	if err != nil {
+// 		t.Fatalf("Unexpected error reading dump: %v", err)
+// 	}
 
-	computedDiff := Diff(
-		precise.GroupedBundleDataChansToMaps(bundle1),
-		precise.GroupedBundleDataChansToMaps(bundle2),
-	)
+// 	computedDiff := Diff(
+// 		precise.GroupedBundleDataChansToMaps(bundle1),
+// 		precise.GroupedBundleDataChansToMaps(bundle2),
+// 	)
 
-	autogold.Equal(t, autogold.Raw(computedDiff))
-}
+// 	autogold.Equal(t, autogold.Raw(computedDiff))
+// }
